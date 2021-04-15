63ba1d1ee7a7033e9009f5b2d6cca7a51c8ab310
FredrikAndre <fredrik.he.andre@gmail.com>
2021-04-15 21:34:39 +0200

    Task 6: Persist text value when refresh

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const buttons = document.querySelectorAll('.clickbutton');
 const boxes = document.querySelectorAll('.component');
 const saveInput = document.querySelector('.word');
 const copyWord = document.querySelector('.repeat_word');
+const savedWord = localStorage.getItem('userInput');
 
 buttons.forEach(button => {
     button.addEventListener('click', function () {
@@ -10,9 +11,15 @@ buttons.forEach(button => {
     })
 })
 
+if (savedWord) {
+    copyWord.textContent = savedWord
+}
+
 saveInput.addEventListener('input', word => {
     copyWord.textContent = word.target.value;
+    localStorage.setItem('userInput', copyWord.textContent);
 })
+
 
 
 
